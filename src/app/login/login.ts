52226cee866566b701819cbed51d7a2fb4fc5bce
MicaelMartins1990 <micael_144@hotmail.com>
2026-06-08 19:09:44 +0100

    Login
    ligacao do login ao backend e criacao da pagina no vscode e da pagina de dashboard

---
+++ src/app/login/login.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router'; 
+import { AuthService } from '../services/auth'; 
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+  rememberMe = false;
+  errorMessage = '';
+
+  // O Angular "injeta" as ferramentas que precisamos aqui no construtor
+  constructor(private authService: AuthService, private router: Router) {}
+
+  handleLogin() {
+    this.errorMessage = '';
+    
+    if (!this.username || !this.password) {
+      this.errorMessage = 'Por favor, preencha todos os campos.';
+      return;
+    }
+
+    const credenciais = {
+      username: this.username,
+      password: this.password
+    };
+
+    // Chamamos o serviço de autenticação para falar com o IntelliJ
+    this.authService.login(credenciais).subscribe({
+      next: (resposta) => {
+        console.log('Login com sucesso!', resposta);
+        this.router.navigate(['/dashboard']); //Salta para o dashboard!
+      },
+      error: (erro) => {
+        console.error('Erro no login:', erro);
+        if (erro.status === 401 || erro.status === 403) {
+          this.errorMessage = 'Utilizador ou palavra-passe incorretos.';
+        } else {
+          this.errorMessage = 'Erro ao contactar o servidor. Tente novamente.';
+        }
+      }
+    });
+  }
+}
